6ed7cad69bac1a4b97bac15ae1db31100934dd53
Alex <al3xchavez@gmail.com>
2025-07-07 17:33:16 -0700

    backgrounds

--- hack_website/src/App.js
+++ hack_website/src/App.js
@@ -1,35 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h className="App-header">
-        <p>
-          Hello and welcome to our website 
-        </p>
-        <p1>Please click one the following links to view our personal bios:</p1>
+      <header className="App-header">
+        <p>Hello and welcome to our website </p>
+        <p>Please click one the following links to view our personal bios:</p>
         <a
           className="App-link"
           href="Alex.html"
         >
           Alex
         </a>
+         <a
+          className="App-link"
+          href="Andrew.html"
+        >
+          Andrew
+        </a>
         <a
         className="App-link"
-          href="Andrew.html"
-          >
-            Andrew
-          </a>
-          <a
-        className="App-link"
           href="Andy.html"
-          >
-            Andy
-          </a>
-      </h>
+        >
+          Andy
+        </a>
+      </header>
     </div>
   );
 }
 
 export default App;
+
+ 
